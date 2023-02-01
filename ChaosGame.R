c7c55d14cf31e196bd87bebe802887691dae1cd7
VladWero08 <vlad_wero@yahoo.com>
2023-02-01 23:05:35 +0200

    Added hexagon generation

--- ChaosGame.R
+++ ChaosGame.R
@@ -46,20 +46,20 @@ ui <- fluidPage(
 # Functions to generate fractals
 
 generateTriangle <- function(ratio){
-  numPoints <- 20000
+  numPoints <- 10000
   initPoints <- matrix(NA, ncol = 2, nrow = 3)
   allPoints <- matrix(NA, ncol = 2, nrow = numPoints)
   
   # In the triangle, in the XY axis, the initial points are:
-  # (0,0), (1,0), (0.5, 1)
+  # (0,0), (4,0), (2, 2)
   initPoints[1, ] <- c(0, 0)
-  initPoints[2, ] <- c(1, 0)
-  initPoints[3, ] <- c(0.5, 1)
+  initPoints[2, ] <- c(4, 0)
+  initPoints[3, ] <- c(2, 2)
   
   # Generate a random point inside the triangle
   # -> this are some boundaries so the random point won't exceed the triangle area
   randX <- runif(1, min = 0.25 ,max = 0.75) 
-  randY <- runif(1, min = 0, max = 0.4)
+  randY <- runif(1, min = 0, max = 0.5)
   
   allPoints[1, ] <- c(randX, randY)
   # For every iteration generate a random vertex of the triangle
@@ -70,6 +70,37 @@ generateTriangle <- function(ratio){
     # ( allPoints[i,1], allPoints[i,2]) --> (x,y) coordinates from the previous point
     # (initPoints[initPointsRand[i], 1], initPoints[initPointsRand[i], 2]) --> random generated coordinates 
     # towards one vertex of the triangle
+    randX <- (initPoints[initPointsRand[i], 1] - allPoints[i, 1]) * ratio + allPoints[i, 1]
+    randY <- (initPoints[initPointsRand[i], 2] - allPoints[i, 2]) * ratio + allPoints[i, 2]
+    
+    allPoints[i + 1, ] <- c(randX, randY)
+  }
+  
+  return (list(initPoints, allPoints))
+}
+
+generateHexagon <- function(ratio){
+  numPoints <- 10000
+  initPoints <- matrix(NA, ncol = 2, nrow = 6)
+  allPoints <- matrix(NA, ncol = 2, nrow = numPoints)
+  
+  # In the hexagon, in the XY axis, the initial points are:
+  # base: (1, 0), (3, 0); middle: (0, 1), (4, 1); top: (1, 2), (3, 2)
+  initPoints[1, ] <- c(1, 0)
+  initPoints[2, ] <- c(3, 0)
+  initPoints[3, ] <- c(0, 1)
+  initPoints[4, ] <- c(4, 1)
+  initPoints[5, ] <- c(1, 2)
+  initPoints[6, ] <- c(3, 2)
+  
+  randX <- runif(1, min = 1.05 ,max = 1.95) 
+  randY <- runif(1, min = 0.05, max = 1.95)
+  allPoints[1, ] <- c(randX, randY)
+  
+  # For every iteration generate a random vertex of the triangle
+  initPointsRand <- sample(1:6, numPoints, replace = TRUE)
+  
+  for(i in 1:(numPoints-1)){
     randX <- (initPoints[initPointsRand[i], 1] - allPoints[i, 1]) * ratio + allPoints[i, 1]
     randY <- (initPoints[initPointsRand[i], 2] - allPoints[i, 2]) * ratio + allPoints[i, 2]
     
@@ -102,7 +133,10 @@ server <- function(input, output){
   })
 
   chosenShape <- reactive({
-    if(input$SelectShape == "triangle"){ return (generateTriangle(input$ChaosRatio))} 
+    if(input$SelectShape == "triangle"){ 
+      return (generateTriangle(input$ChaosRatio))}
+    if(input$SelectShape == "hexagon"){ 
+      return (generateHexagon(input$ChaosRatio))} 
   })
   
   output$mainPlot <- renderPlot({
@@ -110,7 +144,7 @@ server <- function(input, output){
     tips <- chosenShape()[[1]]
     allPoints <- chosenShape()[[2]]
     
-    plot(0, 0, xlim = c(0, 1), ylim = c(0, 1), col = 0, yaxt = "n", xaxt = "n", xlab = "", ylab = "", bty = "n")
+    plot(0, 0, xlim = c(0, 4), ylim = c(0, 2), col = 0, yaxt = "n", xaxt = "n", xlab = "", ylab = "", bty = "n")
     points(allPoints[1 : input$SliderNrPoints, 1], allPoints[1 : input$SliderNrPoints, 2], pch = 46, col = "#D6D5A8")  
     points(tips[, 1], tips[, 2], pch = 20, cex = 3, col = "white")
   })
